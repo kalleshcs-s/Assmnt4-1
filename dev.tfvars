2774c6da3b485f05fd3514ac488a74b8c5256d4c
kalleshcs <kalleshcs@gmail.com>
2025-12-24 20:46:34 +0530

    My second update

--- dev.tfvars
+++
@@ -1,6 +0,0 @@
-project_name   = "myapp"
-env            = "dev"
-subnet_numbers = [1, 2]
-azs            = ["ap-south-1a", "ap-south-1b"]
-ami_id         = "ami-00c6455c6c39f8322"
-create_ec2     = true
--- main.tf
+++ main.tf
@@ -1,79 +1,31 @@
-terraform {
-  required_version = ">= 1.5.0"
+provider "aws" {
+  region = "us-south-1"
+}
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "basic-vpc"
   }
 }
 
-provider "aws" {
-  region = var.aws_region
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-south-1a"
+
+  tags = {
+    Name = "basic-subnet"
+  }
 }
 
-# -----------------------
-# VPC
-# -----------------------
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
-  tags = merge(
-    var.default_tags,
-    {
-      Name = "${var.project_name}-vpc-${var.env}"
-    }
-  )
-}
-
-# -----------------------
-# Subnets using cidrsubnet()
-# -----------------------
-resource "aws_subnet" "subnets" {
-  count                   = length(var.subnet_numbers)
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = cidrsubnet(var.vpc_cidr, 4, var.subnet_numbers[count.index])
-  availability_zone       = var.azs[count.index]
-
-  tags = merge(
-    var.default_tags,
-    {
-      Name = "${var.project_name}-subnet-${count.index + 1}-${var.env}"
-    }
-  )
-}
-
-# -----------------------
-# EC2 Instance (conditional)
-# -----------------------
 resource "aws_instance" "ec2" {
-  count         = var.create_ec2 ? 1 : 0
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.subnets[0].id
+  ami           = "ami-0d55b018c95fe9bfd"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main.id
 
-  tags = merge(
-    var.default_tags,
-    {
-      Name = "${var.project_name}-ec2-${var.env}"
-    }
-  )
-}
-
-# -----------------------
-# S3 Bucket
-# -----------------------
-resource "aws_s3_bucket" "bucket" {
-  bucket = "${var.project_name}-bucket-${var.env}-${random_id.bucket_id.hex}"
-
-  tags = merge(
-    var.default_tags,
-    {
-      Environment = var.env
-    }
-  )
-}
-
-resource "random_id" "bucket_id" {
-  byte_length = 2
+  tags = {
+    Name = "basic-ec2"
+  }
 }
